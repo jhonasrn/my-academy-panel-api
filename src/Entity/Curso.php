7653e3272900e0cd4883aa9843886b6a8df9728d
Jhonas Nascimento <jhonasnascimento@MacBook-Air-de-Leonardo.local>
2025-08-29 15:40:03 -0300

    update branch

---
+++ src/Entity/Curso.php
@@ -0,0 +1,81 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\CursoRepository;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: CursoRepository::class)]
+class Curso
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $titulo = null;
+
+    #[ORM\Column(type: Types::TEXT)]
+    private ?string $descricao = null;
+
+    #[ORM\Column]
+    private ?bool $ativo = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $nivel = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getTitulo(): ?string
+    {
+        return $this->titulo;
+    }
+
+    public function setTitulo(string $titulo): static
+    {
+        $this->titulo = $titulo;
+
+        return $this;
+    }
+
+    public function getDescricao(): ?string
+    {
+        return $this->descricao;
+    }
+
+    public function setDescricao(string $descricao): static
+    {
+        $this->descricao = $descricao;
+
+        return $this;
+    }
+
+    public function isAtivo(): ?bool
+    {
+        return $this->ativo;
+    }
+
+    public function setAtivo(bool $ativo): static
+    {
+        $this->ativo = $ativo;
+
+        return $this;
+    }
+
+    public function getNivel(): ?string
+    {
+        return $this->nivel;
+    }
+
+    public function setNivel(string $nivel): static
+    {
+        $this->nivel = $nivel;
+
+        return $this;
+    }
+}
